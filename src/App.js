50211502745fa2d3705d118bee7089102e900ee4
yotamos5699 <m.lhafakot@gmail.com>
2022-11-24 17:59:50 +0200

    דד

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import { useState, useEffect } from "react";
 
-const url = "http://localhost:3001";
 let toFetch = true;
+
+const reqData = {
+  matrixID: "9321b6eb28c9c3b035bef8984863b2dd887d98d9ad89ea4c9112d3021da3da19",
+  matrixName: "מטריצה3",
+  matrixesData: {
+    mainMatrix: {
+      matrixID: "9321b6eb28c9c3b035bef8984863b2dd887d98d9ad89ea4c9112d3021da3da19",
+      ActionID: [1, 1],
+      AccountKey: ["6026", "6027"],
+      DocumentID: [1, 1],
+      DriverID: ["qewr135256edrfh", "qewr135256edrfh"],
+      ActionAutho: ["Default", "Default"],
+      itemsHeaders: ["HI250SA", "SX250SA", "AB500SA"],
+      itemsNames: ["הרנה 250 גרם", "גת SPXP", "אבו מיסמר גדול"],
+      cellsData: [
+        [3, 2, 1],
+        [1, 0, 0],
+      ],
+    },
+    changesMatrix: {
+      matrixConfig: null,
+      matrixGlobalData: null,
+      cellsData: [
+        [null, null, null],
+        [null, null, null],
+      ],
+      docData: [null, null],
+      metaData: [null, null],
+    },
+  },
+  Date: "11/24/2022, 10:01:09 AM",
+  isBI: true,
+  isInitiated: true,
+};
 
 function App() {
   const [progressStatus, setProgressStatus] = useState("starting....");
+  //const [createDocs, setCreateDocs] = useState();
   useEffect(() => {
     if (toFetch) {
+      fetchDocs();
       fetchStream();
+
       toFetch = false;
     }
     return () => {
@@ -15,12 +51,45 @@ function App() {
     };
   }, []);
 
+  const fetchDocs = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("mode", "no-cors");
+    myHeaders.append(
+      "Authorization",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmZXRjaGVkRGF0YSI6eyJzdGF0dXMiOiJ5ZXMiLCJjb25maWdPYmoiOnsiTW9kdWxzUHJlbWlzc2lvbiI6eyJCaXppUm91dGVzIjp7Im10eFByZWZlcmVuY2VzIjp7ImlzRGVmYXVsdCI6ZmFsc2V9LCJpc0FsbG93Ijp0cnVlLCJkcml2ZXJzIjp7ImlzRGVmYXVsdCI6ZmFsc2UsIkFjY291bnRLZXkiOjF9fSwiTWVzc2FnZXMiOnsid2hhdHNBcHAiOnsiaXNBbGxvdyI6dHJ1ZSwicmVtYWluaW5nU3VtIjoyMjJ9fX0sIm10eENvbmZpZyI6eyJkb2N1bWVudERlZiI6eyJpc0RlZmF1bHQiOnRydWUsIkRvY3VtZW50TnVtYmVyIjoiMiJ9LCJkb2NMaW1pdCI6eyJpc0xpbWl0ZWQiOmZhbHNlLCJBbW91bnQiOjB9LCJzdW1MaW1pdCI6eyJpc0xpbWl0ZWQiOmZhbHNlLCJBbW91bnQiOjB9LCJ0YXhEb2NzIjp7IlJlZnVuZCI6eyJpc0FsbG93Ijp0cnVlLCJpc0xpbWl0ZWQiOmZhbHNlLCJBbW91bnQiOjB9LCJEaXNjb3VudCI6eyJpc0FsbG93Ijp0cnVlLCJpc0xpbWl0ZWQiOmZhbHNlLCJBbW91bnQiOjB9LCJPYmxpZ29QYXNzIjpmYWxzZSwiaXNBbGxvdyI6dHJ1ZX19LCJSZXBvcnRzIjp7ImRlZmF1bHRSZXBvcnRzIjp7ImNhc3R1bWVycyI6eyJzb3J0aW5nS2V5Ijoi16fXldeTINee15nXldefIiwic29ydGluZ1ZhbHVlIjoiMzAwIn0sInByb2R1Y3RzIjp7InNvcnRpbmdLZXkiOiLXnteX16HXnyIsInNvcnRpbmdWYWx1ZSI6IjEifX19LCJFcnBDb25maWciOnsiZXJwTmFtZSI6IkhBIiwiQ29tcGFueUtleSI6IjIzZTU0YjRiM2U1NDEyNjExNDBiZGViMjU3NTM4YmExMWM1MTA0NjIwZTYxMjE3ZDVkNjczNWEzYzkzNjFhNWFhYzY3YTdmODUyNzhlNGU1M2YzMDA4NTk4ZDg5MjdmNjhlODllM2UxNjE0N2MxOTRmOTY5NzZiZGYzMDc1ZDU1IiwiQ29tcGFueVNlcnZlciI6ImxiMTEud2l6Y2xvdWQuY28uaWwiLCJDb21wYW55RGJOYW1lIjoid2l6ZGIyMzk0bjUifSwiX2lkIjoiNjM3MTFmN2MyYWNlMTUyMzM3MWMwY2M1IiwidXNlcklEIjoiNjJmZDBjZWVlZGJjODdiYWYzOTc5NzU3IiwiQWNjb3VudFN0YXRlIjoi16DXmted15HXnNen15IiLCJjcmVhdGVkQXQiOiIyMDIyLTExLTEzVDE2OjQ2OjUyLjI0OVoiLCJ1cGRhdGVkQXQiOiIyMDIyLTExLTEzVDE2OjQ3OjIyLjI0NFoiLCJfX3YiOjB9LCJ1c2VySUQiOiI2MmZkMGNlZWVkYmM4N2JhZjM5Nzk3NTcifSwiaWF0IjoxNjY5MjI0MjYxfQ.9FpSBNBHNbToFXph5KZV9YQaZJXlQjj0wBzd8o1pPjk"
+    );
+    myHeaders.append("filename", "progress");
+
+    const raw = JSON.stringify(reqData);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    return fetch("http://localhost:3000/api/createdoc", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
   const fetchStream = () => {
-    fetch(url, {
+    fetch("http://localhost:3000/api/getProgressBar", {
+      method: "post",
+
       headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmZXRjaGVkRGF0YSI6eyJzdGF0dXMiOiJ5ZXMiLCJjb25maWdPYmoiOnsidXNzZXJJRCI6eyJyZXF1aXJlZCI6dHJ1ZX0sIkRlZmF1bHREcml2ZXIiOnsiaXNEZWZhdWx0IjpmYWxzZX0sIkRvY3VtZW50RGVmIjp7ImlzRGVmYXVsdCI6dHJ1ZSwiRG9jdW1lbnREZWYiOjEsImlzRmlyc3QiOmZhbHNlfSwiUHJlbWlzc2lvbk10eCI6eyJkb2NMaW1pdCI6eyJpc0xpbWl0ZWQiOnRydWUsIkFtb3VudCI6NTB9LCJzdW1MaW1pdCI6eyJpc0xpbWl0ZWQiOnRydWUsIkFtb3VudCI6MjAwMDB9LCJ0YXhEb2NzIjp0cnVlLCJSZWZ1bmQiOnsiaXNBbGxvdyI6ZmFsc2V9LCJEaXNjb3VudCI6eyJpc0FsbG93Ijp0cnVlLCJpc0xpbWl0ZWQiOmZhbHNlfSwiT2JsaWdvUGFzcyI6eyJpc0FsbG93IjpmYWxzZX0sIkZsYWdlZENhc3R1bWVycyI6eyJpc0FsbG93IjpmYWxzZX19fSwidXNlcklEIjoiNjJmZDBjZWVlZGJjODdiYWYzOTc5NzU3In0sImlhdCI6MTY2MjAyMjU1OX0.eACq69czOOlYd0Y8yDNszPylA-uHdsuujJU1A-KUmp4",
+
         "Content-Type": "text/event-stream",
         "Cache-Control": "no-cache",
         Connection: "keep-alive",
+        mode: "no-cors",
+        fileName: "progress",
+        timeLimit: 100,
       },
     })
       .then((response) => response.body)
@@ -39,11 +108,12 @@ function App() {
                   controller.close();
                   return;
                 }
-                // Get the data and send it to the browser via the controller
+                // Get the data and send it to the bowser via the controller
                 controller.enqueue(value);
                 // Check chunks by logging to the console
                 let decodedValue = new TextDecoder().decode(value);
                 setProgressStatus(decodedValue);
+
                 console.log(done, decodedValue);
                 push();
               });
@@ -60,7 +130,7 @@ function App() {
         }).text()
       )
       .then((result) => {
-        // Do things with result
+        // Do things with reult
         console.log(result);
       });
   };
--- src/server.js
+++ src/server.js
@@ -13,22 +13,90 @@ app.use(
 );
 app.use(express.json());
 
-app.listen(PORT, (err) =>
-  console.log(`matrix UI server ${err ? " on" : "listening"} port ${PORT}`)
-);
+app.listen(PORT, (err) => console.log(`matrix UI server ${err ? " on" : "listening"} port ${PORT}`));
 
-app.get(
-  "/",
+const progressBar = async (filename, progressData) => {
+  console.log("IN PROGRESS BAR --", { filename, progressData });
+  const data = progressData;
+  let path = "./" + filename + ".json";
+  if (fs.existsSync(path)) fs.writeFileSync(path, JSON.stringify(data), { encoding: "utf8", flag: "w" });
+  else fs.writeFileSync(path, JSON.stringify(data), { encoding: "utf8" });
+};
+
+app.post("/api/post2", async (req, res) => {
+  const Filename = req.headers["filename"];
+  const TimeLimit = parseInt(req.headers["timelimit"]);
+
+  console.log("IN GET PROGRESS DATA ", { Filename, TimeLimit });
+  let path = "./" + Filename + ".json";
+
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+  const { id } = await req.body;
+  res.status(200);
+  let runtime = 0;
+  let stageName = "";
+  let intervals = setInterval(() => {
+    console.log("IN GET PROGRESS DATA ", { Filename, TimeLimit, runtime });
+    let file_exist = fs.existsSync(path);
+    console.log({ file_exist });
+    if (file_exist) {
+      let result = JSON.parse(fs.readFileSync(path, { encoding: "utf8", flag: "r" }));
+      console.log("file exist");
+
+      if (result.stageName == "finish") {
+        toBreak = true;
+        clearInterval(intervals);
+        res.write("finish");
+        fs.unlinkSync(path);
+        return res.end();
+      }
+     
+      if (result.stageName != stageName) {
+        stageName = result.stageName;
+        res.write(JSON.stringify(result));
+        console.log("file updated");
+      }
+   
+    }
+
+    runtime += 1;
+
+    let limit = TimeLimit ? TimeLimit * 2 : 60;
+   
+    if (runtime > limit) {
+      res.write("to long");
+      file_exist && fs.unlinkSync(path);
+      clearInterval(intervals);
+      return res.end(" out");
+    }
+  }, 500);
+});
+
+app.post(
+  "/api/post",
   async (req, res) => {
+    console.log("in post !!!");
+
     let mssages = ["a", "b", "c", "d", "finish", "wewewe"];
     let num = 0;
-    res.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      Connection: "keep-alive",
-    });
-    const fileStream = fs.createReadStream(`sample-data.txt`);
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    //   "Cache-Control": "no-cache",
+    //   Connection: "keep-alive",
+    // });
 
+    // console.log(req);
+    let Headers = req.headers;
+    let body = await req.body;
+    console.log({ body });
+    console.log("post ", { Headers });
+    //const fileStream = fs.createReadStream(`sample-data.txt`);
+    let Filename = req.headers["filename"];
+    console.log("IN CCREATE DOCS ", { Filename });
     const streamTest = setInterval(() => {
       let progressData = {
         stageName: mssages[num],
@@ -38,11 +106,11 @@ app.get(
           totalToProcess: 4,
         },
       };
-      res.write(JSON.stringify(progressData));
+      progressBar(Filename, progressData);
       if (num == 4) {
         clearInterval(streamTest);
         console.log("finished");
-        return res.end();
+        return res.send({ status: "yes" });
       }
       num += 1;
       console.info("action number ", num, " ", progressData);
